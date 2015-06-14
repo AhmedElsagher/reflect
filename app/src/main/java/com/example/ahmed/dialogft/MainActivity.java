30f6b9a9566e8b27ddab70307c1a9c94ade2ed39
AhmedElsagher <ahmed0elsagher@gmail.com>
2015-06-14 11:34:13 +0200

    erasing ovride methods

--- app/src/main/java/com/example/ahmed/dialogft/MainActivity.java
+++ app/src/main/java/com/example/ahmed/dialogft/MainActivity.java
@@ -31,38 +31,5 @@ TextView tv;
      m.show(fm,"MyD");
  }
 
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
+ /// erase any overide methods
 }
